962dd1e07d2254f243bb80da85d1086bded1a799
Alvaro Reyes Figueroa <alvaroreyesfigueroa@MacBook-Pro-de-Alvaro.local>
2021-01-24 17:04:43 -0300

    Agrega modelo de usuario.

--- app/core/models.py
+++ app/core/models.py
@@ -1,12 +1,24 @@
 from django.db import models
-from django.contrib.auth.models import AbstractBaseUser, BaseUserManager, PermissionsMixin
+from django.contrib.auth.models import AbstractBaseUser, BaseUserManager,PermissionsMixin
 
 class UserManager(BaseUserManager):
 
     def create_user(self, email , password=None, **extra_fields):
         """Creates and saves a new user"""
-        user = self.model(email=email, **extra_fields)
+        if not email:
+            raise ValueError('User must have an email address')
+        user = self.model(email=self.normalize_email(email), **extra_fields)
         user.set_password(password)
+        user.save(using=self._db)
+
+        return user
+
+
+    def create_superuser(self, email, password):
+        """Creates and save a new super user"""
+        user = self.create_user(email,password)
+        user.is_staff = True
+        user.is_superuser = True
         user.save(using=self._db)
 
         return user
