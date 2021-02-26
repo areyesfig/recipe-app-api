769ddc34b3b1a2fbac543c0d3f1792b8d2df1f1a
Alvaro Reyes Figueroa <alvaroreyesfigueroa@MacBook-Pro-de-Alvaro.local>
2021-02-26 17:11:06 -0300

    Configured postgres db.

--- app/core/admin.py
+++ app/core/admin.py
@@ -9,20 +9,20 @@ class UserAdmin(BaseUserAdmin):
     ordering = ['id']
     list_display = ['email', 'name']
     fieldsets = (
-    (None, {'fields': ('email','password')}),
-    (_('Personal Info'), {'fields': ('name',)}),
-    (
-        _('Permissions'),
-        {
-            'fields': (
-                'is_active',
-                'is_staff',
-                'is_superuser',
-             )
-        }
-    ),
-    (_('Important dates'), {'fields': ('last_login',)})
-)
+        (None, {'fields': ('email', 'password')}),
+        (_('Personal Info'), {'fields': ('name',)}),
+        (
+            _('Permissions'),
+            {
+                'fields': (
+                    'is_active',
+                    'is_staff',
+                    'is_superuser',
+                )
+            }
+         ),
+        (_('Important dates'), {'fields': ('last_login',)})
+    )
 
     add_fieldsets = (
         (None, {
@@ -30,8 +30,6 @@ class UserAdmin(BaseUserAdmin):
             'fields': ('email', 'password1', 'password2')
         }),
     )
-
-
 
 
 admin.site.register(models.User, UserAdmin)
--- app/core/test/test_admin.py
+++ app/core/test/test_admin.py
@@ -38,4 +38,4 @@ class AdminSiteTest(TestCase):
         url = reverse('admin:core_user_add')
         res = self.client.get(url)
 
-        self.assertEqual(res.status_code,200)
+        self.assertEqual(res.status_code, 200)
