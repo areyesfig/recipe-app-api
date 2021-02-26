330266c3be7405c611e599e77b998383524c329b
Alvaro Reyes Figueroa <alvaroreyesfigueroa@MacBook-Pro-de-Alvaro.local>
2021-02-25 22:21:17 -0300

    update admin to support custom model.

--- app/core/models.py
+++ app/core/models.py
@@ -1,9 +1,10 @@
 from django.db import models
-from django.contrib.auth.models import AbstractBaseUser, BaseUserManager,PermissionsMixin
+from django.contrib.auth.models import AbstractBaseUser, BaseUserManager, PermissionsMixin
+
 
 class UserManager(BaseUserManager):
 
-    def create_user(self, email , password=None, **extra_fields):
+    def create_user(self, email, password=None, **extra_fields):
         """Creates and saves a new user"""
         if not email:
             raise ValueError('User must have an email address')
@@ -13,10 +14,9 @@ class UserManager(BaseUserManager):
 
         return user
 
-
     def create_superuser(self, email, password):
         """Creates and save a new super user"""
-        user = self.create_user(email,password)
+        user = self.create_user(email, password)
         user.is_staff = True
         user.is_superuser = True
         user.save(using=self._db)
@@ -30,7 +30,5 @@ class User(AbstractBaseUser, PermissionsMixin):
     name = models.CharField(max_length=255)
     is_active = models.BooleanField(default=True)
     is_staff = models.BooleanField(default=False)
-
     objects = UserManager()
-
     USERNAME_FIELD = 'email'
