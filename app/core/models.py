d8ddafaaeb509865db9b95d3cc0a2af5a9e5e40f
Alvaro Reyes Figueroa <alvaroreyesfigueroa@MacBook-Pro-de-Alvaro.local>
2021-03-10 18:03:15 -0300

    test ingredients  for private and public

--- app/core/models.py
+++ app/core/models.py
@@ -45,3 +45,14 @@ class Tag(models.Model):
 
     def __str__(self):
         return self.name
+
+class Ingredient(models.Model):
+    """Ingrediente to be used in recipe"""
+    name = models.CharField(max_length=255)
+    user = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE
+    )
+
+    def __str__(self):
+        return self.name
